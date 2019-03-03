767fc84b8cbb08a7cd5c19a38d1e91739fd4a641
Liang Guo <liang@smartly.io>
2019-03-03 23:55:41 +0200

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome #{@user.name}!"
-      redirect_to @user
+      redirect_to @user # short for redirect_to user_url(@user)
     else
       render 'new'
     end
