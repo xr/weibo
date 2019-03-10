74e1dadd53332abeffdf81eb78ab8c26cb9d62af
Liang Guo <liang@smartly.io>
2019-03-10 14:56:20 +0200

    done without production sendgrid

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,13 +27,24 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = self.send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  # 激活账户
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  # 发送激活邮件
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 
   private
